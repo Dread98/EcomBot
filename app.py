03be6995cf89a4e00112bd4b5ca069e83042d776
Kieran <kieran.b@live.co.uk>
2024-05-05 21:55:20 +0100

    Created the intentIdentifier and called it within the main script to set up the three requirements to come

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from utils import get_response
+from intentIdentifier import identify_intent_from_message
 
 app = Flask(__name__)
-
 
 @app.get("/")
 def index_get():
@@ -12,8 +12,18 @@ def index_get():
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
+    intent = identify_intent_from_message(text)
+    # if intent == "tracking":
+    #     trackingConvoStage = 1
+    #     return "of course what is your order tracking number?"
+    # else if intent == "review":
+    #     reviewConvoStage = 1
+    #     return "of course what is your order tracking number?"
+    #
     response = get_response(text)
     message = {"answer": response}
+    # message = {"answer": intent}
+
     return jsonify(message)
 
 
